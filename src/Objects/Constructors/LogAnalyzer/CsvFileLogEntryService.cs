87e97171ca28aa1cae707c173513d78e8e0f4cb4
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2023-11-23 11:51:26 +0100

    Dodanie rozwiązania analizy logu

---
+++ src/Objects/Constructors/LogAnalyzer/CsvFileLogEntryService.cs
@@ -0,0 +1,48 @@
+﻿using LogAnalyzer.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogAnalyzer
+{
+    internal class CsvFileLogEntryService
+    {
+        public List<LogEntry> GetLogEntries(string path, char separator = ',')
+        {
+            // Tworzymy pustą listę
+            List<LogEntry> logEntries = new List<LogEntry>();
+
+            // Tworzymy czytnik strumienia z pliku
+            StreamReader reader = new StreamReader(path);
+
+            // Odczytujemy nagłówek
+            string header = reader.ReadLine();
+
+            // Czytamy strumień tak długo zanim się nie skończy
+            while (!reader.EndOfStream)
+            {
+                // Odczytujemy linię
+                string line = reader.ReadLine();
+
+                // Tniemy linię na kolumny i przypisujemy do tablicy stringów
+                string[] columns = line.Split(separator);
+
+                // Odczytujemy wartości w poszczególnych kolumnach
+                string sourceAddressIp = columns[1];
+                string protocol = columns[3];
+                int bytesTransferred = int.Parse(columns[5]);
+
+                // Mapujemy kolumny na obiekt LogEntry
+                LogEntry logEntry = new LogEntry(sourceAddressIp, protocol, bytesTransferred);
+
+                // Dodajemy wpis do listy
+                logEntries.Add(logEntry);
+            }
+
+            // Zwracamy wynik
+            return logEntries;
+        }
+    }
+}
---
+++ src/Objects/Constructors/LogAnalyzer/Program.cs
@@ -0,0 +1,53 @@
+﻿using LogAnalyzer.Model;
+
+namespace LogAnalyzer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            string path = "ip_traffic_log.csv";
+
+            string[] protocols = ["TCP", "UDP", "HTTP"];
+
+
+            CsvFileLogEntryService logEntryService = new CsvFileLogEntryService();
+            List<LogEntry> logEntries = logEntryService.GetLogEntries(path);
+
+            // Zbiór
+            HashSet<string> uniqueIpAddresses = new HashSet<string>();
+
+            // Linq
+            List<string> uniqueIpAddressesLinq = logEntries // SQL: SELECT * FROM logEntries
+                                                .Select(p=>p.Source_IP) // SQL: SELECT Source_IP FROM logEntries
+                                                .Distinct() // SQL: SELECT DISTINCT Source_IP FROM logEntries
+                                                .ToList();         
+
+            string selectedProtocol;
+
+            do
+            {
+                Console.WriteLine("Podaj protokół: ");
+                Console.WriteLine(string.Join(',', protocols));
+
+                selectedProtocol = Console.ReadLine();
+            }
+            while (!protocols.Contains(selectedProtocol));
+
+
+            foreach (LogEntry logEntry in logEntries)
+            {
+                uniqueIpAddresses.Add(logEntry.Source_IP);
+            }
+
+
+            foreach(string ipAddress in uniqueIpAddresses)
+            {
+                Console.WriteLine(ipAddress);
+            }
+
+        }
+    }
+}
