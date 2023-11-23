7a94326882df947fb2475b13eb5f0be6e72da65c
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2023-11-23 12:21:54 +0100

    Dodanie sumowania

--- src/Objects/Constructors/LogAnalyzer/Program.cs
+++ src/Objects/Constructors/LogAnalyzer/Program.cs
@@ -12,19 +12,17 @@ namespace LogAnalyzer
 
             string[] protocols = ["TCP", "UDP", "HTTP"];
 
-
             CsvFileLogEntryService logEntryService = new CsvFileLogEntryService();
             List<LogEntry> logEntries = logEntryService.GetLogEntries(path);
 
             // Zbiór
-            HashSet<string> uniqueIpAddresses = new HashSet<string>();
+           // HashSet<string> uniqueIpAddresses = new HashSet<string>();
 
-            // Linq
+            // LINQ 
             List<string> uniqueIpAddressesLinq = logEntries // SQL: SELECT * FROM logEntries
                                                 .Select(p=>p.Source_IP) // SQL: SELECT Source_IP FROM logEntries
                                                 .Distinct() // SQL: SELECT DISTINCT Source_IP FROM logEntries
                                                 .ToList();         
-
             string selectedProtocol;
 
             do
@@ -36,17 +34,18 @@ namespace LogAnalyzer
             }
             while (!protocols.Contains(selectedProtocol));
 
+            // LINQ (zapytanie z filtrowaniem)
+            List<string> filteredIpAddressesLinq = logEntries       
+                .Where(p=>p.Protocol == selectedProtocol)
+                .Select(p=>p.Source_IP)
+                .Distinct()
+                .ToList();  // SQL: SELECT DISTINCT Source_IP FROM logEntries WHERE Protocol = 'TCP'
 
-            foreach (LogEntry logEntry in logEntries)
-            {
-                uniqueIpAddresses.Add(logEntry.Source_IP);
-            }
+            // LINQ (zapytanie z sumowaniem)
 
+            int totalBytesTransferred = logEntries
+                .Sum(p => p.Bytes_Transferred); // SQL: SELECT SUM(Bytes_Transferred) FROM logEntries 
 
-            foreach(string ipAddress in uniqueIpAddresses)
-            {
-                Console.WriteLine(ipAddress);
-            }
 
         }
     }
